7951a640a9c595fd59d50c98d700ba910cea6edb
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-04-18 16:15:59 +0300

    Context sınıfı dbsetler

--- EntityLayer/Concrete/SocialMedia.cs
+++ EntityLayer/Concrete/SocialMedia.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class SociallMedia
+    public class SocialMedia
     {
         [Key]
         public int SocialMediaId { get; set; }
