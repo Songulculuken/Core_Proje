b7b4eac236c25d6dd1dbc989894cd101877fd9e2
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-04-18 16:42:05 +0300

    migration

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,17 @@ namespace DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=192.168.100.40;database=CoreProjeDB;integrated security=true"); //bağlantı adresi
+            optionsBuilder.UseSqlServer("server=192.168.100.40;database=CoreProjeDB;Persist Security Info=True;User ID=Logo;Password=JnRpndvJ;"); //bağlantı adresi
         }
+        public DbSet<About> Abouts { get; set; } //Abouts sql tablo ismi. About c# da kullanacağımız isim
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocaialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
