5a407808a67f36ed6979d771b0c356d8db3dc204
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-05-08 12:09:01 +0300

    Entity Framework

---
+++ DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfAboutDal : GenericRepository<About>, IAboutDal // crud operasyonları dışında ayrı bir işlem yapılmak istenirse ve bu işlem sadece ilgili entity'e ait olursa imza IAboutDal'da atılacak. İçi ise bu kısımda doldurulacak. O yüzden IAboutDal kısmı burada miras olarak tanımlanır. 
+    {
+
+    }
+}
