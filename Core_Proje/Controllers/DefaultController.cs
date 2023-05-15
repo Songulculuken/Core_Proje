186b607add1053c60899814c19a06cb01b335850
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-05-15 11:18:44 +0300

    Partial View

--- Core_Proje/Controllers/DefaultController.cs
+++ Core_Proje/Controllers/DefaultController.cs
@@ -8,5 +8,10 @@ namespace Core_Proje.Controllers
         {
             return View();
         }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
     }
 }
