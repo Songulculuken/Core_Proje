92d34655f5fa5a5689b3646caefe33ec8ffd6cc9
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-05-17 13:55:58 +0300

    contact,message

--- DataAccessLayer/EntityFramework/EfContactDal.cs
+++ DataAccessLayer/EntityFramework/EfContactDal.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    internal class EfContactDal : GenericRepository<Contact>, IContactDal 
+    public class EfContactDal : GenericRepository<Contact>, IContactDal 
     {
     }
 }
