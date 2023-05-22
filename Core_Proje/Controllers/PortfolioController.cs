8baf091a66e778ea84de686493512ec882b7f9c4
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-05-22 16:45:35 +0300

    portfolio list and create

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -14,6 +15,20 @@ namespace Core_Proje.Controllers
             ViewBag.d1 = "/Portfolio/Index";
             var values = portfolioManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Proje Ekleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.d1 = "/Portfolio/Index";
+            return View();
+        }
+        [HttpPost]  
+        public IActionResult AddPortfolio(Portfolio p)
+        {
+            portfolioManager.TAdd(p);   
+            return RedirectToAction("Index");   
         }
     }
 }
