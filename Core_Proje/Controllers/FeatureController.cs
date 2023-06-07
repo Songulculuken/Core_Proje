e984b12d9d9328e73edbaa986093fc49614fd78a
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-06-07 10:20:37 +0300

    Feature

--- Core_Proje/Controllers/FeatureController.cs
+++ Core_Proje/Controllers/FeatureController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.v1 = "Öne Çıkanlar Düzenleme";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.d1 = "/Feature/Index";
+            var values = featureManager.TGetById(1);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult Index(Feature feature) 
+        { 
+            featureManager.TUpdate(feature);
+            return RedirectToAction("Index","Default");   
         }
     }
 }
