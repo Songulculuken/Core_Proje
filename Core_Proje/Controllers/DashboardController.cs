54638b416e95244d6372187b79665c7b4ac6cfbe
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-06-12 14:39:39 +0300

    admin template change

---
+++ Core_Proje/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Dashboard";
+            ViewBag.v2 = "İstatistikler";
+            ViewBag.d1 = "/Dashboard/Index";
+            return View();
+        }
+    }
+}
