3fc1c7cca0f247c229ce658db49ca6db50fd26b6
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-06-12 11:45:07 +0300

    Service list,add,delete,update

---
+++ Core_Proje/Controllers/ServiceController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1= "Hizmet Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.d1 = "/Service/Index";
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            ViewBag.v1 = "Hizmet Ekle";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.d1 = "/Service/Index";
+            return View();
+        }
+        [HttpPost]  
+        public IActionResult AddService(Service service)
+        {
+            serviceManager.TAdd(service);   
+            return RedirectToAction("Index");   
+        }
+        public IActionResult DeleteService(int id) 
+        {
+            var values = serviceManager.TGetById(id);
+            serviceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            ViewBag.v1 = "Hizmet Düzenle";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.d1 = "/Service/Index";
+            var values = serviceManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]  
+        public IActionResult EditService(Service service)
+        {
+            serviceManager.TUpdate(service);
+            return RedirectToAction("Index");
+        }
+    }
+}
